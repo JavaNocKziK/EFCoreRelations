97ef5aea3ae75e1b6f03322cb25b407da33840a6
WAU\aubrey.boorer <aubrey.boorer@webapplicationsuk.com>
2018-11-25 10:42:31 +0000

    E

--- EFCoreRelations/PlaceContext.cs
+++ EFCoreRelations/PlaceContext.cs
@@ -28,9 +28,7 @@ namespace EFCoreRelations
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
-        [Required]
-        [MaxLength(50)]
+        
         public ICollection<PersonEntity> People { get; set; }
     }
 }
